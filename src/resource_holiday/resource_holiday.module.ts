ad0134bf2fd5e41ee0142e98739af2a1deae0b2a
Dinithi Wijesooriya <112229654+Dinithiwijesooriya@users.noreply.github.com>
2024-05-30 20:34:19 +0530

    team management

--- src/resource_holiday/resource_holiday.module.ts
+++ src/resource_holiday/resource_holiday.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ResourceHolidayService } from './resource_holiday.service';
 import { ResourceHolidayController } from './resource_holiday.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ResourceHoliday } from './entities/resource_holiday.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ResourceHoliday])],
   controllers: [ResourceHolidayController],
   providers: [ResourceHolidayService]
 })
