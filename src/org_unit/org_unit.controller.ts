5b3fd2193e1eaed8d8607c9009d2cfef5586e333
janithedirisinghe <110591538+janithedirisinghe@users.noreply.github.com>
2024-06-02 13:23:24 +0530

    After fixing some bugs

--- src/org_unit/org_unit.controller.ts
+++ src/org_unit/org_unit.controller.ts
@@ -1,4 +1,14 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Put, ParseIntPipe } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Put,
+  ParseIntPipe,
+} from '@nestjs/common';
 import { OrgUnitService } from './org_unit.service';
 import { CreateOrgUnitDto } from './dto/create-org_unit.dto';
 import { UpdateOrgUnitDto } from './dto/update-org_unit.dto';
@@ -10,7 +20,7 @@ export class OrgUnitController {
   constructor(private orgUnitService: OrgUnitService) {}
 
   @Post()
-  createOrgUnit(@Body() createOrgUnitDto : CreateOrgUnitDto) {
+  createOrgUnit(@Body() createOrgUnitDto: CreateOrgUnitDto) {
     return this.orgUnitService.createOrgUnit(createOrgUnitDto);
   }
 
@@ -24,18 +34,18 @@ export class OrgUnitController {
     return this.orgUnitService.getUnitNameById(unitId);
   }
 
-
   @Put(':unitId')
-  async updateOrgUnitById(@Param('unitId', ParseIntPipe) unitId: number, @Body() updateOrgUnitDto: UpdateOrgUnitDto) {
+  async updateOrgUnitById(
+    @Param('unitId', ParseIntPipe) unitId: number,
+    @Body() updateOrgUnitDto: UpdateOrgUnitDto,
+  ) {
     await this.orgUnitService.updateOrgUnit(unitId, updateOrgUnitDto);
   }
-
 
   @Delete(':unitId')
   async deleteOrgUnitById(@Param('unitId', ParseIntPipe) unitId: number) {
     await this.orgUnitService.deleteOrgUnit(unitId);
   }
-
 
   @Get('hierarchy/data')
   async getOrgUnitHierarchy(): Promise<any> {
@@ -53,8 +63,15 @@ export class OrgUnitController {
   //   return ancestors;
   // }
 
-  @Get(':unitId/ancestors')
-  async getAncestors(@Param('unitId', ParseIntPipe) unitId: number): Promise<OrgUnit[]> {
+  @Get('ancestors/parents/:unitId')
+  async getAncestors(
+    @Param('unitId', ParseIntPipe) unitId: number,
+  ): Promise<OrgUnit[]> {
     return this.orgUnitService.getAncestors(unitId);
+  }
+
+  @Get('parent/:id')
+  async getParentDetails(@Param('id') id: number) {
+    return this.orgUnitService.getParentDetailsRecursive(id);
   }
 }
